4c73eff9003ff35e3c51344caed82434a6a2ae87
Mateusz Werpachowski <matyszxzx@gmail.com>
2017-10-21 17:53:57 +0200

    Added login window

--- Lottery/Lottery.Console/ConsoleWindows/LoginWindow.cs
+++ Lottery/Lottery.Console/ConsoleWindows/LoginWindow.cs
@@ -15,8 +15,80 @@ namespace Lottery.Console.ConsoleWindows
 
         public void Print()
         {
-            System.Console.WriteLine("xd");
-            System.Console.WriteLine(_loginService.LogIn("xd", "dx"));
+            //DrawBorder();
+            DrawMenu();
+            string username = System.Console.ReadLine();
+            System.Console.SetCursorPosition(55, 15);
+            string password = System.Console.ReadLine();
+
+            if (LogInOrRegister())
+                _loginService.LogIn(username, password);
+            //else
+                //TODO: Register(username, password);
+
+        }
+
+        private static void DrawMenu()
+        {
+            System.Console.SetCursorPosition(5, 4);
+            System.Console.Write("Welcome to lottery!");
+            System.Console.SetCursorPosition(20, 14);
+            System.Console.Write("Username:");
+            System.Console.SetCursorPosition(20, 15);
+            System.Console.Write("______________");
+            System.Console.SetCursorPosition(55, 14);
+            System.Console.Write("Password:");
+            System.Console.SetCursorPosition(55, 15);
+            System.Console.Write("______________");
+            System.Console.SetCursorPosition(80, 25);
+            System.Console.Write("LOGIN");
+            System.Console.SetCursorPosition(100, 25);
+            System.Console.Write("REGISTER");
+
+            System.Console.SetCursorPosition(20, 15);
+        }
+
+        private static bool LogInOrRegister()
+        {
+            System.Console.CursorVisible = false;
+            bool isLogin = true;
+            while (true)
+            {
+                if (isLogin)
+                {
+                    System.Console.SetCursorPosition(80, 25);
+                    System.Console.BackgroundColor = ConsoleColor.White;
+                    System.Console.ForegroundColor = ConsoleColor.Black;
+                    System.Console.Write("LOGIN");
+                    System.Console.ResetColor();
+                    System.Console.SetCursorPosition(100, 25);
+                    System.Console.Write("REGISTER");
+                }
+                else
+                {
+                    System.Console.SetCursorPosition(80, 25);
+                    System.Console.Write("LOGIN");
+                    System.Console.SetCursorPosition(100, 25);
+                    System.Console.BackgroundColor = ConsoleColor.White;
+                    System.Console.ForegroundColor = ConsoleColor.Black;
+                    System.Console.Write("REGISTER");
+                    System.Console.ResetColor();
+                }
+                ConsoleKeyInfo cki = System.Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        isLogin = true;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        isLogin = false;
+                        break;
+                    case ConsoleKey.Enter:
+                        return isLogin;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
--- Lottery/Lottery.Console/Program.cs
+++ Lottery/Lottery.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Lottery.Application.Services;
 using Lottery.Console.ConsoleWindows;
+using System;
 
 namespace Lottery.Console
 {
@@ -8,9 +9,35 @@ namespace Lottery.Console
         static void Main(string[] args)
         {
             var loginService = new LoginService();
-
             var loginWindow = new LoginWindow(loginService);
+            DrawBorder();
             loginWindow.Print();
+        }
+        public static void DrawBorder()
+        {
+            System.Console.SetWindowSize(120, 30);
+            for (int lengthCount = 3; lengthCount < 27; lengthCount++)
+            {
+                System.Console.SetCursorPosition(3, lengthCount);
+                System.Console.Write("|");
+                System.Console.SetCursorPosition(116, lengthCount);
+                System.Console.Write("|");
+            }
+            System.Console.SetCursorPosition(4, 2);
+            for (int widthCount = 3; widthCount < 59; widthCount++)
+            {
+                System.Console.Write("- ");
+            }
+            System.Console.SetCursorPosition(4, 6);
+            for (int widthCount = 3; widthCount < 59; widthCount++)
+            {
+                System.Console.Write("- ");
+            }
+            System.Console.SetCursorPosition(4, 27);
+            for (int widthCount = 3; widthCount < 59; widthCount++)
+            {
+                System.Console.Write("- ");
+            }
         }
     }
 }
