516c5dc3ebc1032f240377ec459ac933e1a45d1f
Mateusz Werpachowski <matyszxzx@gmail.com>
2017-11-27 15:41:51 +0100

    Improved filter

--- Lottery/Lottery.Angular/app/filters/filters.js
+++ Lottery/Lottery.Angular/app/filters/filters.js
@@ -1,6 +1,7 @@
 ﻿app.filter('parseDate', function () {
     return function (timestamp) {
         var date = timestamp.split('T')[0] + ' ' + timestamp.split('T')[1];
+        date = date.slice(0, date.lastIndexOf(':'));
         return date;
     };
 });
